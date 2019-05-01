ee3247c7aff56f8df9e1e904f1f38cf43621ff5b
Mateus de Morais <mateusmorais78@gmail.com>
2019-05-01 14:37:15 -0300

    Modify run script and project settings

--- scripts/run/dev.sh
+++ scripts/run/dev.sh
@@ -3,4 +3,6 @@
 sleep 20
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py collectstatic --no-input
 python3 manage.py runserver 0.0.0.0:8000
+# gunicorn --config /gunicorn.conf --log-config /logging.conf -b :8000 patient-management.wsgi
