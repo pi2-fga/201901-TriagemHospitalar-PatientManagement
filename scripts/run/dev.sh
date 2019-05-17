638498306d21bf2d7e0efc1949b39958db052cd7
Ju <130060119@aluno.unb.br>
2019-05-17 17:08:47 -0300

    Update styles and change port django to 3000

--- scripts/run/dev.sh
+++ scripts/run/dev.sh
@@ -4,5 +4,5 @@ sleep 20
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:3000
 # gunicorn --config /gunicorn.conf --log-config /logging.conf -b :8000 patient-management.wsgi
