e842fbd3ab259527667c0e0d14c49782748eaca7
Ju <130060119@aluno.unb.br>
2019-05-20 12:34:09 -0300

    Updates search bar

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('criar-conta/', views.RegistrationView.as_view(), name='sign_up'),
     path('efetuar-login/', views.sign_in, name='sign_in'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,36 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic.edit import FormView
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-from .forms import RegistrationForm
-
-
-class RegistrationView(FormView):
-    template_name = 'sign_up.html'
-    form_class = RegistrationForm
-    success_url = '/'
-
-    def post(self, request):
-
-        form = RegistrationForm(request.POST)
-
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-
-            response = redirect(self.success_url)
-        else:
-            response = render(request, self.template_name, {'form': form})
-
-        return response
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        return super().form_valid(form)
-
+from django.shortcuts import render
 
 def sign_in(request):
     return render(request, 'sign_in.html')
