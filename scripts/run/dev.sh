5a0835e193f55c8204c475994d9e3df054d45f2f
Mateus de Morais <mateusmorais78@gmail.com>
2019-05-31 11:00:08 -0300

    Adds users, patients and triages fixtures

--- scripts/run/dev.sh
+++ scripts/run/dev.sh
@@ -4,5 +4,8 @@ sleep 20
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
+python3 manage.py loaddata core/fixtures/users.json
+python3 manage.py loaddata core/fixtures/patients.json
+python3 manage.py loaddata core/fixtures/triages.json
 python3 manage.py runserver 0.0.0.0:3000
 # gunicorn --config /gunicorn.conf --log-config /logging.conf -b :8000 patient-management.wsgi
