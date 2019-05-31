52921e4ca558082fd83b100129a9e31cf8679046
Mateus de Morais <mateusmorais78@gmail.com>
2019-05-30 22:32:59 -0300

    Adds clerk queue mechanism

--- patient-management/urls.py
+++ patient-management/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', include('core.routes')),
     path('autenticacao/', include('users.routes')),
     path('paciente/', include('consultations.routes')),
     path('admin/', admin.site.urls),
--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('efetuar-login/', views.sign_in, name='sign_in'),
-]
