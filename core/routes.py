c21b3c1b51525882aa735dd0286706a43e768f9a
Mateus de Morais <mateusmorais78@gmail.com>
2019-05-31 08:19:16 -0300

    Adds initial medic queue mechanism

--- core/routes.py
+++ core/routes.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from boogie.router import Router
 from consultations.models.emergency_care import Triage
@@ -16,7 +17,7 @@ def index(request):
 
         if hasattr(request.user, 'medic'):
 
-            response = redirect('/medic')
+            response = redirect('/chamada-de-pacientes/')
 
         elif hasattr(request.user, 'clerk'):
 
@@ -85,6 +86,71 @@ def patient_register(request):
         response = render(
             request,
             'clerk_homepage.html',
+            {
+                'next_patient': next_patient,
+                'triage_queue_list': triage_queue_list
+            }
+        )
+
+    else:
+
+        response = redirect('/')
+
+    return response
+
+
+@urlpatterns.route('chamada-de-pacientes/')
+def patient_call(request):
+    """
+    [...]
+    """
+
+    if hasattr(request.user, 'medic') or request.user.is_superuser:
+
+        triage_queue = Triage.objects.filter(
+            ~Q(patient=None)
+        ).order_by(
+            'risk_level',
+            'created_at'
+        )
+
+        try:
+            next_patient = triage_queue.first().id
+        except AttributeError:
+            next_patient = None
+
+        triage_queue_list = list()
+
+        for index, triage in enumerate(triage_queue):
+
+            triage_information = dict()
+            triage_information['position'] = index+1
+            triage_information['name'] = triage.name
+            triage_information['age'] = triage.age
+            triage_information['date_hour'] = triage.created_at
+            triage_information['risk'] = dict()
+            triage_information['risk']['background_color'] = \
+                Triage.TRIAGE_RISK_CATEGORIES[triage.risk_level][1]
+
+            if triage.risk_level == 0:
+                triage_information['risk']['text'] = 'VERMELHO'
+                triage_information['risk']['text_color'] = 'white'
+            elif triage.risk_level == 1:
+                triage_information['risk']['text'] = 'AMARELO'
+                triage_information['risk']['text_color'] = 'black'
+
+            elif triage.risk_level == 2:
+                triage_information['risk']['text'] = 'VERDE'
+                triage_information['risk']['text_color'] = 'white'
+            elif triage.risk_level == 3:
+                triage_information['risk']['text'] = 'AZUL'
+                triage_information['risk']['text_color'] = 'white'
+
+            triage_queue_list.append(triage_information)
+
+        response = render(
+            request,
+            'medic_homepage.html',
             {
                 'next_patient': next_patient,
                 'triage_queue_list': triage_queue_list
